f4393883f92e8efbf7f0f1a862ac5a91d9db72b1
CristianErick <CristianErick@users.noreply.github.com>
2026-07-31 08:37:47 -0500

    Modificacion de cors.php

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,8 @@ return [
     'http://localhost:5000',
     'http://127.0.0.1:5000',
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://frontend-web-gastos.vercel.app/'
 ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
